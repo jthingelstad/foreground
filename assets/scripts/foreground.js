bf544024e5269909600014cedf6e3eb1ef329aa5
Garrick van Buren <email@garrickvanburen.com>
2013-06-11 11:12:54 -0500

    reverted .section > .title a from, added js to split namespace titles into labels

--- assets/scripts/foreground.js
+++ assets/scripts/foreground.js
@@ -1,3 +1,6 @@
 jQuery(document).ready(function() {
   jQuery(document).foundation();
+  var $title = jQuery('h3:contains(":")').text().split(":");
+  jQuery('h3:contains(":")').html('<small class="label">' + $title[0] + '</small>' + $title[1]);
+  jQuery('#mw-normal-catlinks ul li a').addClass('label');
 });
