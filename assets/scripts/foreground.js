047ecd799afa56435e55c6eb5903221a900ed941
Garrick van Buren <email@garrickvanburen.com>
2013-06-11 14:45:48 -0500

    adding comments to js file

--- assets/scripts/foreground.js
+++ assets/scripts/foreground.js
@@ -1,6 +1,11 @@
 jQuery(document).ready(function() {
   jQuery(document).foundation();
   var $title = jQuery('h3:contains(":")').text().split(":");
+
+  // Split the namespace title into a title and a label
   jQuery('h3:contains(":")').html('<small class="label">' + $title[0] + '</small>' + $title[1]);
+
+  // Turn categories into labels
   jQuery('#mw-normal-catlinks ul li a').addClass('label');
+
 });
