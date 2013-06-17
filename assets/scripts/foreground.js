0634fde5363389f59bdc20c02aa709e35d13d879
Garrick van Buren <email@garrickvanburen.com>
2013-06-17 13:54:26 -0500

    changing h2.small.label to inline-block to keep it from spanning the entire width, and adding the clear:both to the :after css call

--- assets/scripts/foreground.js
+++ assets/scripts/foreground.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
   jQuery(document).foundation();
   // Split the namespace title into a title and a label
   var $title = jQuery("h2:contains(':')").not(":contains('\"')").text().split(":");
-  jQuery("h2:contains(':')").not(":contains('\"')").html('<small class="label">' + $title[0] + '</small><div class="clear_both"></div>' + $title[1]);
+  jQuery("h2:contains(':')").not(":contains('\"')").html('<small class="label">' + $title[0] + '</small>' + $title[1]);
 
   // Turn categories into labels
   jQuery('#mw-normal-catlinks ul li a').addClass('label');
