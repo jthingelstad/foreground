2e4932c6b9681b05710eaac91db88a111729998c
Garrick van Buren <email@garrickvanburen.com>
2013-06-17 16:28:59 -0500

    attempting to force hour minute and second fields in WikiApiary to width:auto, and wrapping a <span> around the h2s to force the label above the pagetitle.

--- assets/scripts/foreground.js
+++ assets/scripts/foreground.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
   jQuery(document).foundation();
   // Split the namespace title into a title and a label
   var $title = jQuery("h2:contains(':')").not(":contains('\"')").not(":contains('as of')").text().split(":");
-  jQuery("h2:contains(':')").not(":contains('\"')").not(":contains('as of')").html('<small class="label">' + $title[0] + '</small>' + $title[1]);
+  jQuery("h2:contains(':')").not(":contains('\"')").not(":contains('as of')").html('<small class="label">' + $title[0] + '</small><span>' + $title[1] + '</span>');
 
   // Turn categories into labels
   jQuery('#mw-normal-catlinks ul li a').addClass('label');
