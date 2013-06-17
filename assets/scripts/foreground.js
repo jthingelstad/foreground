c5231a7417206da169475d4d3e79eeb21378b66e
Garrick van Buren <email@garrickvanburen.com>
2013-06-17 15:44:54 -0500

    updating JS to ignore revision datetime when turning namespaces into labels

--- assets/scripts/foreground.js
+++ assets/scripts/foreground.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function() {
   jQuery(document).foundation();
   // Split the namespace title into a title and a label
-  var $title = jQuery("h2:contains(':')").not(":contains('\"')").text().split(":");
-  jQuery("h2:contains(':')").not(":contains('\"')").html('<small class="label">' + $title[0] + '</small>' + $title[1]);
+  var $title = jQuery("h2:contains(':')").not(":contains('\"')").not(":contains('as of')").text().split(":");
+  jQuery("h2:contains(':')").not(":contains('\"')").not(":contains('as of')").html('<small class="label">' + $title[0] + '</small>' + $title[1]);
 
   // Turn categories into labels
   jQuery('#mw-normal-catlinks ul li a').addClass('label');
