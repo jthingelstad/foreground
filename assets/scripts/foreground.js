dc32594cb709dd52bb24eb7d866f7a69d5e229f1
Garrick van Buren <email@garrickvanburen.com>
2013-06-17 09:13:55 -0500

    fixed js issues in the H2 tag namespace re-writing, added Jamie's standard title hiding CSS

--- assets/scripts/foreground.js
+++ assets/scripts/foreground.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function() {
   jQuery(document).foundation();
   // Split the namespace title into a title and a label
-  var $title = jQuery('h2:not(:contains("\""))').jQuery('h2:contains(":")').text().split(":");
-  jQuery('h2:not(:contains("\""))').jQuery('h2:contains(":")').html('<small class="label">' + $title[0] + '</small><div class="clear_both"></div>' + $title[1]);
+  var $title = jQuery("h2:contains(':')").not(":contains('\"')").text().split(":");
+  jQuery("h2:contains(':')").not(":contains('\"')").html('<small class="label">' + $title[0] + '</small><div class="clear_both"></div>' + $title[1]);
 
   // Turn categories into labels
   jQuery('#mw-normal-catlinks ul li a').addClass('label');
