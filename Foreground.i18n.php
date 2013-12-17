fd7aafc277e4ceaeb60ae64a42069f7fe08154dd
ardiman <sobotnik@gmx.net>
2013-12-17 16:05:03 +0100

    Some new language strings
    For message if old ie is used and menu title for visitors with smartphones

--- Foreground.i18n.php
+++ Foreground.i18n.php
@@ -14,7 +14,9 @@ $messages = array();
  */
 $messages['en'] = array(
 	'skinname-foreground' => 'Foreground',
-	'foreground-desc' => 'Provides a skin that focuses on putting your content in the foreground'
+	'foreground-desc' => 'Provides a skin that focuses on putting your content in the foreground',
+	'foreground-browsermsg' => 'may not look as expected in this version of Internet Explorer. We recommend you upgrade to a newer version of Internet Explorer or switch to a browser like Firefox or Chrome.',
+	'foreground-menutitle' => 'Menu'
 );
 
 /** German (Deutsch)
@@ -22,5 +24,7 @@ $messages['en'] = array(
  */
 $messages['de'] = array(
 	'skinname-foreground' => 'Foreground',
-	'foreground-desc' => 'Stellt eine Benutzeroberfläche bereit, die die Inhalte des Wikis betont'
+	'foreground-desc' => 'Stellt eine Benutzeroberfläche bereit, die die Inhalte des Wikis betont',
+	'foreground-browsermsg' => 'sieht in dieser Version des Internet Explorers nicht wie gewünscht aus. Es wird ein Upgrade des Internet Explorers oder der Umstieg auf einen Browser wie Firefox oder Chrome empfohlen.',
+	'foreground-menutitle' => 'Menü'
 );
