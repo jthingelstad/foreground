519f67cc4117ee16687171ad62a76da9615c3257
Hayden Bickerton <haydenbbickerton@gmail.com>
2014-06-15 21:28:47 -0500

    Fix for the tabs

--- Foreground.skin.php
+++ Foreground.skin.php
@@ -65,6 +65,17 @@ class foregroundTemplate extends BaseTemplate {
 				echo "<div id='navwrapper' class='". $wgForegroundFeatures['NavWrapperType']. "'>";
 				break;
 		}
+		
+		ob_start();
+		$this->html('bodytext'); 
+		$out = ob_get_contents();
+		ob_end_clean();
+		$remove1 = str_replace("&lt;a", "<a", $out);
+		$remove2 = str_replace("&lt;/a", "</a", $remove1);
+		$remove3 = str_replace("&gt;", ">", $remove2);
+		$newbody = $remove3;
+		
+		
 ?>
 <!-- START FOREGROUNDTEMPLATE -->
 		<nav class="top-bar" data-topbar>
@@ -174,7 +185,10 @@ class foregroundTemplate extends BaseTemplate {
 					<?php if ( $this->data['isarticle'] ) { ?><h3 id="tagline"><?php $this->msg( 'tagline' ) ?></h3><?php } ?>
 					<h5 class="subtitle"><?php $this->html('subtitle') ?></h5>
 					<div class="clear_both"></div>
-					<?php $this->html('bodytext') ?>
+					<?php 
+					echo $newbody;
+					ob_flush();
+					 ?>
 		    	<div class="group"><?php $this->html('catlinks'); ?></div>
 		    	<?php $this->html('dataAfterContent'); ?>
 		    </div>
