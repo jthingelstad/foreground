870d3af17f4236f9e9d5f039da93a2e31460f72d
paladox2015 <paladox2015@users.noreply.github.com>
2014-06-20 11:14:37 +0100

    Update foundation.alerts.js

--- assets/scripts/foundation/foundation.alerts.js
+++ assets/scripts/foundation/foundation.alerts.js
@@ -1,52 +1,43 @@
-/*jslint unparam: true, browser: true, indent: 2 */
-
 ;(function ($, window, document, undefined) {
   'use strict';
 
-  Foundation.libs.alerts = {
-    name : 'alerts',
+  Foundation.libs.alert = {
+    name : 'alert',
 
-    version : '4.0.0',
+    version : '5.3.0',
 
     settings : {
-      speed: 300, // fade out speed
       callback: function (){}
     },
 
     init : function (scope, method, options) {
-      this.scope = scope || this.scope;
-
-      if (typeof method === 'object') {
-        $.extend(true, this.settings, method);
-      }
-
-      if (typeof method != 'string') {
-        if (!this.settings.init) this.events();
-
-        return this.settings.init;
-      } else {
-        return this[method].call(this, options);
-      }
+      this.bindings(method, options);
     },
 
     events : function () {
-      var self = this;
+      var self = this,
+          S = this.S;
 
-      $(this.scope).on('click.fndtn.alerts', '[data-alert] a.close', function (e) {
+      $(this.scope).off('.alert').on('click.fndtn.alert', '[' + this.attr_name() + '] a.close', function (e) {
+          var alertBox = S(this).closest('[' + self.attr_name() + ']'),
+              settings = alertBox.data(self.attr_name(true) + '-init') || self.settings;
+
         e.preventDefault();
-        $(this).closest("[data-alert]").fadeOut(self.speed, function () {
-          $(this).remove();
-          self.settings.callback();
-        });
+        if (Modernizr.csstransitions) {
+          alertBox.addClass("alert-close");
+          alertBox.on('transitionend webkitTransitionEnd oTransitionEnd', function(e) {
+            S(this).trigger('close').trigger('close.fndtn.alert').remove();
+            settings.callback();
+          });
+        } else {
+          alertBox.fadeOut(300, function () {
+            S(this).trigger('close').trigger('close.fndtn.alert').remove();
+            settings.callback();
+          });
+        }
       });
-
-      this.settings.init = true;
-    },
-
-    off : function () {
-      $(this.scope).off('.fndtn.alerts');
     },
 
     reflow : function () {}
   };
-}(Foundation.zj, this, this.document));
+}(jQuery, window, window.document));
