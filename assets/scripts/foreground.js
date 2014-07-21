8f687d27d759fa7c1e54b39b24046e8630bf7a1d
paladox2015 <paladox2015@users.noreply.github.com>
2014-07-21 19:34:26 +0100

    Update foreground.js

--- assets/scripts/foreground.js
+++ assets/scripts/foreground.js
@@ -1,14 +1,9 @@
+jQuery(document).foundation();
 
 jQuery(document).ready(function() {
   // Add the 'less than IE9' class to appropriate version of IE by checking for their support of cssFloat (true in v9)
   if (!jQuery.support.cssFloat) { jQuery('html').addClass('lt-ie9').addClass('no-js'); }
 
-
-  jQuery(document).foundation(function (response) {
-    // console.log(response.errors); < this line will produce error in ie9!
-    if (window.console) console.log(response.errors);
-  });
-  
   // The Echo extension puts an item in personal tools that Foreground really should have in the top menu
   // to make this easier, we move it here and loaded earlier to speed up transform
   jQuery("#pt-notifications").prependTo("#echo-notifications");
