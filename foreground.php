3824073bfe041cd78d43432da27c77aa52027a43
paladox <paladox@users.noreply.github.com>
2015-12-11 08:29:57 +0000

    Update foreground.php

--- foreground.php
+++ foreground.php
@@ -26,7 +26,7 @@ $wgExtensionCredits['skin'][] = array(
 	'path'		 => __FILE__,
 	'name'		 => 'Foreground',
 	'url'		 => 'http://foreground.thingelstad.com/',
-	'version'	 => '1.2',
+	'version'	 => '1.2.0',
 	'author'	 => array(
 		'Garrick Van Buren',
 		'Jamie Thingelstad',
