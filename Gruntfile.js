b2599ae403e86ddc5fe178e37359953f0aa63880
paladox <paladox@users.noreply.github.com>
2015-12-16 16:33:29 +0000

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	// grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
 
 	grunt.initConfig( {
 		jshint: {
@@ -17,6 +18,9 @@ module.exports = function ( grunt ) {
 				'!assets/scripts/vendor/**'
 			]
 		},
+		jscs: {
+			src: '<%= jshint.all %>'
+		},
 		/*banana: conf.MessagesDirs,*/
 		jsonlint: {
 			all: [
@@ -26,6 +30,6 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jshint', 'jsonlint' /* 'banana' */ ] );
+	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint' /* 'banana' */ ] );
 	grunt.registerTask( 'default', 'test' );
 };
